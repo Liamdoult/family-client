4bfcd047db6a1e9fbba475cae5962d4a4443e3f1
LiamDoult <3579535@myuwc.ac.za>
2020-11-10 12:11:09 +0100

    Basic application framework
    - Get box by id
    - Suggestion system for items

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+
+import Search from './Search';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    hight: "100vh",
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
+
+interface Item {
+  _id: string,
+  name: string,
+  description: string,
+  owner: string | undefined,
+  quantity: Number | undefined,
+  created: Date,
+}
+
+interface Box {
+  _id: string,
+  items: Item[],
+  location: string,
+  created: Date,
+  updated: Date[],
+}
+
+
+
+function App() {
+  const classes = useStyles();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [box, setBox] = useState<Box|undefined>(undefined);
+
+  function handleSubmit(boxId: string) {
+    setLoading(true);
+    fetch(`http://localhost:8080/storage/box?id=${boxId}`).then(res => {
+      res.json().then(json => {
+        console.log(json);
+        setBox(json);
+      })
+    }).catch(err => {
+      console.log(err);
+      setLoading(false);
+    });
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Search hook={handleSubmit} disabled={loading}></Search>
+            </Paper>
+            {
+              box !== undefined? 
+              <Paper className={classes.paper}>
+                {box._id}<br />
+                {box.location}<br />
+                {box.created}<br />
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Description</TableCell>
+                        <TableCell align="right">Created</TableCell>
+                        <TableCell align="right">Owner</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {box.items.map((item: Item) => (
+                        <TableRow key={item._id}>
+                          <TableCell component="th" scope="row">
+                            {item.name}
+                          </TableCell>
+                          <TableCell align="right">{item.description}</TableCell>
+                          <TableCell align="right">{item.created}</TableCell>
+                          <TableCell align="right">{item.owner || ""}</TableCell>
+                          <TableCell align="right">{item.quantity || 1}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>:<></>
+            }
+        </Grid>
+      </Grid>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+  underline: {
+    "&&&:before": {
+      borderBottom: "none"
+    },
+    "&&:after": {
+      borderBottom: "none"
+    }
+  }
+}));
+
+interface SearchProps {
+    hook: (value: string) => any,
+    disabled?: boolean,
+}
+
+interface Item {
+  _id: string,
+  name: string,
+  description: string,
+  owner: string | undefined,
+  quantity: Number | undefined,
+  created: Date,
+}
+
+
+export default function Search(props: SearchProps) {
+  const classes = useStyles();
+  const [ value, setValue ] = useState<string>("");
+  const [ loading, setLoading ] = useState<boolean>(false);
+  const [ options, setOptions ] = useState<string[]>([]);
+
+  const { hook, disabled } = props;
+
+  useEffect(() => {
+    if (value === "") return;
+    setLoading(true);
+    (async () => {
+      const res = await fetch(`http://localhost:8080/storage/search?term=${value}`);
+      const json = await res.json();
+      let results: string[] = [];
+      if (json.items) results = results.concat(json.items.map((item: Item) => item.name));
+      setOptions(results);
+      setLoading(false);
+    })()
+  }, [value]);
+
+  return (
+    <Autocomplete
+      id="search-ac"
+      freeSolo
+      // open={true}
+      options={options}
+      loading={loading}
+      getOptionLabel={(option) => option}
+      filterOptions={(options, state) => options}
+      style={{ width: 400 }}
+      renderInput={(params: any) => (
+        <Paper component="form" className={classes.root}>
+            <TextField
+                {...params}
+                className={classes.input}
+                placeholder="Search"
+                onChange={ event => setValue(event.target.value) }
+                autoComplete="off"
+            />
+            <IconButton onClick={ () => hook(value) } className={classes.iconButton} aria-label="search" disabled={disabled}>
+                <SearchIcon />
+            </IconButton>
+        </Paper>
+      )}
+    />
+  );
+}
